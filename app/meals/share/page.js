5ecaec5e8279809ffae267282a8e7f27effe125d
ziadessamcr7 <ziadessamcr7@gmail.com>
2026-01-07 13:40:33 +0200

    handle error message location

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,10 +1,14 @@
+'use client'
+
 import { ImagePicker } from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { submitForm } from '@/lib/actions';
 import MealsFromSubmit from '@/components/meals/meals-form-submit';
+import { useFormState } from 'react-dom';
 
 export default function ShareMealPage() {
 
+  const [state, formAction] = useFormState(submitForm, { message: null })
 
 
   return (
@@ -16,7 +20,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={submitForm}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -48,6 +52,7 @@ export default function ShareMealPage() {
             label={"Your image"}
             name={"image"}
           />
+          {state.message && <p> {state.message} </p>}
           <p className={classes.actions}>
             {/* <button type="submit">Share Meal</button> */}
             <MealsFromSubmit />
